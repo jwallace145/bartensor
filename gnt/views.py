cacc7994a1d1918dbb954def5d4b33eccad5bb1e
JP Dahms <jack.dahms@gmail.com>
2020-01-22 15:59:44 -0500

    added login for the mockup

--- gnt/views.py
+++ gnt/views.py
@@ -1,21 +1,34 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .models import User
 
 def index(request):
-    return render(request, 'gnt/index.html')
+    logged_in = len(User.objects.filter(username='big_duddy666')) > 0
+    for user in User.objects.all():
+        user.delete()
+    return render(request, 'gnt/index.html', {
+            'logged_in': logged_in,
+        })
 
 def signup(request):
+    u = User(username='big_duddy666')
+    u.save()
     if request.method == 'POST':
-        # TODO: Fill in correct url for redirect
-        return redirect('/')
+        # TODO sign up user here
+        return HttpResponseRedirect(reverse('gnt:index'))
     else:
         return render(request, 'gnt/signup.html')
 
 def login(request):
+    u = User(username='big_duddy666')
+    u.save()
     if request.method == 'POST':
-        # TODO: Fill in correct url for redirect
-        return redirect('/')
+        # TODO: login user here
+        return HttpResponseRedirect(reverse('gnt:index'))
     else:
         return render(request, 'gnt/login.html')
 
