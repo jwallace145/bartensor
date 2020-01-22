e2d6272e60a7272d18bd8b0bf09777e70dc087cf
Michael Huang <huang.2721@osu.edu>
2020-01-21 21:08:59 -0500

    created login and signup html

--- gnt/views.py
+++ gnt/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'gnt/index.html')
+
+
+def login(request):
+    return render(request, 'gnt/login.html')
+
+
+def signup(request):
+    return render(request, 'gnt/signup.html')
