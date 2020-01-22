c95988dcc9ec0f3df0c83bb6f320ad46e74843c5
Michael Huang <huang.2721@osu.edu>
2020-01-22 10:10:52 -0500

    Prepped the views for login/signup for redirect

--- gnt/views.py
+++ gnt/views.py
@@ -1,14 +1,23 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'gnt/index.html')
 
 def signup(request):
-    return render(request, 'gnt/signup.html')
+    if request.method == 'POST':
+        # TODO: Fill in correct url for redirect
+        return redirect('/')
+    else:
+        return render(request, 'gnt/signup.html')
 
 def login(request):
-    return render(request, 'gnt/login.html')
+    if request.method == 'POST':
+        # TODO: Fill in correct url for redirect
+        return redirect('/')
+    else:
+        return render(request, 'gnt/login.html')
 
 def results(request):
     return render(request, 'gnt/results.html')
