e4f7116224e2125531369ca9b7307ce48246deea
Eric-Thomas <bluedevils027@gmail.com>
2020-02-03 22:24:04 -0500

    set up test.html for creating drinks with a loop

--- gnt/views.py
+++ gnt/views.py
@@ -7,39 +7,42 @@ from django.urls import reverse
 from .models import User
 
 def index(request):
-    return render(request, 'gnt/index.html', {
-            'logged_in': len(User.objects.filter(username='big_duddy666')) > 0,
-        })
+	return render(request, 'gnt/index.html', {
+			'logged_in': len(User.objects.filter(username='big_duddy666')) > 0,
+		})
 
 def signup(request):
-    u = User(username='big_duddy666')
-    u.save()
-    if request.method == 'POST':
-        # TODO sign up user here
-        return HttpResponseRedirect(reverse('gnt:index'))
-    else:
-        return render(request, 'gnt/signup.html')
+	u = User(username='big_duddy666')
+	u.save()
+	if request.method == 'POST':
+		# TODO sign up user here
+		return HttpResponseRedirect(reverse('gnt:index'))
+	else:
+		return render(request, 'gnt/signup.html')
 
 def login(request):
-    u = User(username='big_duddy666')
-    u.save()
-    if request.method == 'POST':
-        # TODO: login user here
-        return HttpResponseRedirect(reverse('gnt:index'))
-    else:
-        return render(request, 'gnt/login.html')
+	u = User(username='big_duddy666')
+	u.save()
+	if request.method == 'POST':
+		# TODO: login user here
+		return HttpResponseRedirect(reverse('gnt:index'))
+	else:
+		return render(request, 'gnt/login.html')
 
 def logout(request):
-    for user in User.objects.all():
-        user.delete()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+	for user in User.objects.all():
+		user.delete()
+	return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def results(request):
-    return render(request, 'gnt/results.html', {
-            'logged_in': len(User.objects.filter(username='big_duddy666')) > 0,
-        })
+	return render(request, 'gnt/results.html', {
+			'logged_in': len(User.objects.filter(username='big_duddy666')) > 0,
+		})
 
 def loading(request):
-    return render(request, 'gnt/loading.html', {
-            'logged_in': len(User.objects.filter(username='big_duddy666')) > 0,
-        })
+	return render(request, 'gnt/loading.html', {
+			'logged_in': len(User.objects.filter(username='big_duddy666')) > 0,
+		})
+
+def test(request):
+	return render(request, 'gnt/test.html')
