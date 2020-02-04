5a05b16a1aa7dd25723224499bfb14b9ac8fd424
Eric Thomas <bluedevils027@gmail.com>
2020-02-04 18:33:43 -0500

    renamed anon func and added dislike drink logic

--- gnt/static/scripts/results.js
+++ gnt/static/scripts/results.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
    var anchor = $(".thumbsup");
-   anchor.each(function anonymouse_founction(index, element) {
+   anchor.each(function likeDrink(index, element) {
    		$(this).on('click', function likeDrink() {
    			var html_id = $(this).attr('id');
-   			console.log(html_id.split('-').slice(-1).pop());
+   			var drink_id= html_id.split('-').slice(-1).pop();
+   		});
+   });
+});
+
+$(document).ready(function() {
+   var anchor = $(".thumbsdown");
+   anchor.each(function dislikeDrink(index, element) {
+   		$(this).on('click', function likeDrink() {
+   			var html_id = $(this).attr('id');
+   			var drink_id= html_id.split('-').slice(-1).pop();
    		});
    });
 });
