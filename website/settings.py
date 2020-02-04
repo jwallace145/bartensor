dce5dc6427569cb6ef372cd3b696f072781abf11
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-02-04 16:55:07 -0500

    refactored login/logout to use django built in views and added jenkins compatibility files

--- website/settings.py
+++ website/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gnt.apps.GntConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
