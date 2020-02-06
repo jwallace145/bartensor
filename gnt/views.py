551b1cd8f564ac3625b5961994e113c1132bbf88
Eric-Thomas <bluedevils027@gmail.com>
2020-02-05 21:43:44 -0500

    Moved glass into method

--- gnt/views.py
+++ gnt/views.py
@@ -41,12 +41,12 @@ def results(request):
 			'id': 'ankeoigab4a34q35htrgif847qyrahd',
 			'picture': 'https://assets.bonappetit.com/photos/57acc14e53e63daf11a4d9b6/master/pass/whiskey-sour.jpg',
 			'name': 'Whiskey Sour',
-			'glass': 'Highball',
 			'ingredients': ['1 ½ oz oz Whiskey',
 							'2 oz Sour Mix (Fresh preferred)',
 							'Optional: 1/2 oz egg white (makes drink foamy)'
 							],
-			'method': ['Shake Ingredients in a Mixing Glass or Cocktail Shaker w/ice',
+			'method': ['glass': 'Highball',
+						'Shake Ingredients in a Mixing Glass or Cocktail Shaker w/ice',
 						'Strain into a large old fashioned glass with fresh ice',
 						'Garnish with cherry & orange'
 						]
@@ -56,14 +56,14 @@ def results(request):
 			'id': '394agdnavior;oa4eih',
 			'picture': 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9h-WqGmZDxZTkvjEqRWyDG0ZIw5wC6RlQyFj_THX8fmYcQEgv',
 			'name': 'Old Fashioned',
-			'glass': 'Rocks glass',
 			'ingredients': ['Packet of Sugar', 
 							'2 dashes of Bitters', 
 							'Splash of soda',
 							'Cherry & orange',
 							'2 oz Whiskey'
 							],
-			'method': ['Muddle sugar, bitters, soda in glass',
+			'method': [	'glass': 'Rocks glass',
+						'Muddle sugar, bitters, soda in glass',
 						'Add Whiskey',
 						'Fill glass with ice'
 						]
