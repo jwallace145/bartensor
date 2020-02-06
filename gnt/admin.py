f5ead3418e84847cad9bb88f993ff1d867901f27
calebcodesgud <43999129+calebcodesgud@users.noreply.github.com>
2020-02-06 14:07:31 -0500

    new models and users app integration

--- gnt/admin.py
+++ gnt/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import Profile, Drinks, Drink_names
+
+admin.site.register(Profile)
+admin.site.register(Drinks)
+admin.site.register(Drink_names)
--- website/urls.py
+++ website/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from users import views as user_views
 from gnt import views as gnt_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,10 +26,10 @@ urlpatterns = [
     path('home', gnt_views.home, name='home'),
     path('results/', gnt_views.results, name='results'),
     path('loading/', gnt_views.loading, name='loading'),
-    path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/', user_views.profile, name='profile'),
+    path('register/', gnt_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='gnt/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='gnt/logout.html'), name='logout'),
+    path('profile/', gnt_views.profile, name='profile'),
 ]
 
 if settings.DEBUG:
