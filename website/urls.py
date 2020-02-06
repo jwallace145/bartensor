46a6e5921f386117f37f02301567c506e938db51
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-02-06 14:22:27 -0500

    github action test - syntax error in urls.py on purpose

--- website/urls.py
+++ website/urls.py
@@ -33,6 +33,8 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
 ]
 
+lkjhsf
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
