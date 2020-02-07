5ff252827bb6ec90a45758c51367242dfee05849
JP Dahms <jack.dahms@gmail.com>
2020-02-07 15:54:05 -0500

    cleaned drinks.json

---
+++ gnt/static/data/data-split/clean.py
@@ -0,0 +1,20 @@
+import json
+
+dirty = json.loads(open('drinks.json').read())
+
+i = 1
+clean = []
+for dirty_drink in dirty.values():
+    if dirty_drink['name'][0].strip() == '':
+        continue
+    clean_drink = {
+        'id': i,
+        'names': dirty_drink['name'],
+        'ingredients': dirty_drink['ingredients'],
+        'method': dirty_drink['method']
+    }
+    i += 1
+    clean.append(clean_drink)
+
+with open('drinks-cleaned.json', 'w') as f:
+    f.write(json.dumps(clean, indent=4))
