34d5fc0ab8ed12cea1b49dc0962dc5f5bc3e5d98
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-02-12 14:36:02 -0500

    made watson discovery key a settings variable that grabs from the environment vars and refactored the run bash script

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # export environment variables
-export BARTENSOR_EMAIL_USERNAME=
-export BARTENSOR_EMAIL_PASSWORD=
+export BARTENSOR_EMAIL_USERNAME=bartensor@gmail.com
+export BARTENSOR_EMAIL_PASSWORD=iupeqdduwlekqjrj
+export WATSON_DISCOVERY_API_KEY=Jc1KWt03zHYFzwvVf3_UVOyFpdagyO7P8GU-9ra9_8cy
 
 # set run script variables
 DJANGO_PORT=8000
@@ -30,40 +31,44 @@ then
   fi
 fi
 
-echo "virtual environment detected..."
-
-# ensure python version == 3.8
-PYTHON_VERSION="$(python --version)"
-echo "current python version -> $PYTHON_VERSION"
-
-if [[ "$PYTHON_VERSION" == "Python 3.8.0" ]]
+# ensure user is in a virtual env
+if [[ "$VIRTUAL_ENV" != "" ]]
 then
-  echo "current python version is up to date -> $PYTHON_VERSION"
+  echo "virtual environment detected..."
 
-  PIP_VERSION="$(pip --version)"
-  echo "current pip version -> $PIP_VERSION"
-  python -m pip install --upgrade pip
-
-  PIP_VERSION="$(pip --version)"
-  echo "upgraded pip version -> $PIP_VERSION"
-
-  echo "install the required dependencies..."
-  pip install -r requirements.txt
-  echo "pip has installed all required dependencies..."
-
-  echo "django make migrations..."
-  python manage.py makemigrations
-
-  echo "django migrate..."
-  python manage.py migrate
-
-  echo "starting server at localhost port $DJANGO_PORT"
-  python manage.py runserver $DJANGO_PORT
-else
-  echo "python version not up to date..."
+  # ensure python version == 3.8
+  PYTHON_VERSION="$(python --version)"
   echo "current python version -> $PYTHON_VERSION"
-  echo "update python version to execute this script..."
-  echo "exiting bash script now..."
 
-  exit 1
+  if [[ "$PYTHON_VERSION" == "Python 3.8.0" ]]
+  then
+    echo "current python version is up to date -> $PYTHON_VERSION"
+
+    PIP_VERSION="$(pip --version)"
+    echo "current pip version -> $PIP_VERSION"
+    python -m pip install --upgrade pip
+
+    PIP_VERSION="$(pip --version)"
+    echo "upgraded pip version -> $PIP_VERSION"
+
+    echo "install the required dependencies..."
+    pip install -r requirements.txt
+    echo "pip has installed all required dependencies..."
+
+    echo "django make migrations..."
+    python manage.py makemigrations
+
+    echo "django migrate..."
+    python manage.py migrate
+
+    echo "starting server at localhost port $DJANGO_PORT"
+    python manage.py runserver $DJANGO_PORT
+  else
+    echo "python version not up to date..."
+    echo "current python version -> $PYTHON_VERSION"
+    echo "update python version to execute this script..."
+    echo "exiting bash script now..."
+
+    exit 1
+  fi
 fi
