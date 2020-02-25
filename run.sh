ac05919f2edd30047fabf30aa4ce26cc27be2e17
Jimmy Wallace <wallace.901@osu.edu>
2020-02-25 18:56:27 -0500

    add requirements-windows.txt to resolve windows os dependency issues

--- run.sh
+++ run.sh
@@ -103,7 +103,7 @@ then
   echo "WATSON_DISCOVERY_API_KEY = $WATSON_DISCOVERY_API_KEY"
 
   echo "install the required dependencies..."
-  pip install -r requirements.txt
+  pip install -r requirements-windows.txt
   echo "pip has installed all required dependencies..."
 
   echo "django make migrations..."
