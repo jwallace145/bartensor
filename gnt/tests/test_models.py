07c3b03dd81a2c062b9c8a498e0efaeb08e67baa
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-03-15 17:30:14 -0400

    added test coverage support and some test cases

--- gnt/tests/test_models.py
+++ gnt/tests/test_models.py
@@ -1,13 +1,40 @@
+"""
+Model Tests
+"""
+
+# import necessary modules
 from django.test import TestCase
 from django.contrib.auth.models import User
 from gnt.models import Profile
 
+# testing constants
+TEST_USERNAME = 'test_username'
+TEST_EMAIL = 'test_email'
+TEST_PASSWORD = 'test_password'
+TEST_IMAGE = 'test_image'
+TEST_BIO = 'test_bio'
 
-# profile model test
-class ProfileTest(TestCase):
 
-    def create_user(self, username='test_username', email='test_email'):
-        return User.create(username=username, email=email)
+class UserModelTest(TestCase):
+    """
+    User Model Test
+    """
 
-    def create_profile(self, user, image, bio)
-    return 1
+    def setUp(self):
+        """
+        User Model Test Set Up
+        """
+
+        User.objects.create(username=TEST_USERNAME,
+                            email=TEST_EMAIL, password=TEST_PASSWORD)
+
+    def test_retrieve_user(self):
+        """
+        Test Retrieve User
+        """
+
+        user = User.objects.get(username=TEST_USERNAME)
+        self.assertTrue(isinstance(user, User))
+        self.assertEqual(TEST_USERNAME, user.username)
+        self.assertEqual(TEST_EMAIL, user.email)
+        self.assertEqual(TEST_PASSWORD, user.password)
