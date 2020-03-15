abf59c654758ccdab944e66a2a2ff46c34aebe16
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-03-15 17:57:39 -0400

    excluded test classes from coverage reports

--- gnt/tests/test_models.py
+++ gnt/tests/test_models.py
@@ -11,7 +11,7 @@ from gnt.models import Profile
 TEST_USERNAME = 'test_username'
 TEST_EMAIL = 'test_email'
 TEST_PASSWORD = 'test_password'
-TEST_IMAGE = 'test_image'
+TEST_IMAGE = 'default.jpg'
 TEST_BIO = 'test_bio'
 
 
@@ -20,21 +20,36 @@ class UserModelTest(TestCase):
     User Model Test
     """
 
-    def setUp(self):
+    def test_create_and_save_user(self):
         """
-        User Model Test Set Up
+        Test Create and Save User
         """
-
         User.objects.create(username=TEST_USERNAME,
                             email=TEST_EMAIL, password=TEST_PASSWORD)
 
-    def test_retrieve_user(self):
-        """
-        Test Retrieve User
-        """
-
         user = User.objects.get(username=TEST_USERNAME)
+
         self.assertTrue(isinstance(user, User))
         self.assertEqual(TEST_USERNAME, user.username)
         self.assertEqual(TEST_EMAIL, user.email)
         self.assertEqual(TEST_PASSWORD, user.password)
+
+
+class ProfileModelTest(TestCase):
+    """
+    Profile Model Test
+    """
+
+    def test_create_and_save_profile(self):
+        user = User.objects.create(username=TEST_USERNAME,
+                                   email=TEST_EMAIL, password=TEST_PASSWORD)
+
+        profile = Profile.objects.get(user=user)
+        profile.image = TEST_IMAGE
+        profile.bio = TEST_BIO
+        profile.save()
+
+        self.assertTrue(isinstance(profile, Profile))
+        self.assertEqual(user, profile.user)
+        self.assertEqual(TEST_IMAGE, profile.image)
+        self.assertEqual(TEST_BIO, profile.bio)
