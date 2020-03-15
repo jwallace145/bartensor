9ae8736211b5ca1d60b6a328ee56f2b9fe38bf3d
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-03-15 13:04:42 -0400

    refactored website/urls.py module to improve pylint score

--- website/urls.py
+++ website/urls.py
@@ -1,25 +1,15 @@
-"""website URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+Website URL Configuration Module
+"""
+
+# import necessary modules
 from django.contrib import admin
 from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
-from gnt import views as gnt_views
-import gnt.db_api as gnt_db_api
 from django.conf import settings
 from django.conf.urls.static import static
+from gnt import views as gnt_views
+import gnt.db_api as gnt_db_api
 
 
 urlpatterns = [
