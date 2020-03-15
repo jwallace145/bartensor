616a037c890b1280cf06fcf15b7d9ff080929845
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-03-15 12:49:14 -0400

    refactored views.py module to improve pylint score

--- gnt/views.py
+++ gnt/views.py
@@ -1,32 +1,42 @@
-from django.shortcuts import render, redirect
+"""
+Views Module
+"""
+
+# import necessary modules
 from django.contrib import messages
-from django.http import HttpResponse
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, CreateUserDrinkForm, CreateUserDrinkIngredientForm, CreateUserDrinkInstructionForm
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect, JsonResponse
-from django.urls import reverse
 from django.contrib.auth.models import User
-from django.conf import settings
-from .models import Profile, Drink, DrinkName, ProfileToLikedDrink, ProfileToDislikedDrink, Friend, FriendRequest, UserDrink
-from ibm_watson import DiscoveryV1
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from django.forms.formsets import formset_factory
-from .stt import IBM
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from gnt.adapters import drink_adapter
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, CreateUserDrinkForm, CreateUserDrinkIngredientForm, CreateUserDrinkInstructionForm
+from .models import Profile, Drink, ProfileToLikedDrink, ProfileToDislikedDrink, Friend, FriendRequest, UserDrink
+from .stt import IBM
 
-# get api key from settings.py which is stored as an environment variable
-api_key = getattr(settings, 'WATSON_DISCOVERY_API_KEY', None)
 
+def bad_request(request):
+    """
+    Bad Request View
+    """
 
-def bad_request(request, *args, **kwargs):
     return HttpResponseRedirect(reverse('home'))
 
 
 def home(request):
+    """
+    Home View
+    """
+
     return render(request, 'gnt/index.html')
 
 
 def results(request):
+    """
+    Results View
+    """
+
     if request.method == 'POST':
         if 'audio' in request.FILES:
             audio = request.FILES['audio']
@@ -46,26 +56,36 @@ def results(request):
 
 
 def register(request):
+    """
+    Register View
+    """
+
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
 
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             messages.success(
                 request, f'Your account has been created! You are now able to log in.')
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'gnt/register.html', {'form': form})
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'gnt/register.html', context)
 
 
 @login_required
 def profile_create_drink(request):
+    """
+    Profile Create Drink View
+    """
+
     IngredientFormset = formset_factory(CreateUserDrinkIngredientForm)
     InstructionFormset = formset_factory(CreateUserDrinkInstructionForm)
-
-    profile = request.user
 
     if request.method == 'POST':
         create_user_drink_form = CreateUserDrinkForm(request.POST)
@@ -91,7 +111,8 @@ def profile_create_drink(request):
                     instruction.drink = drink
                     instruction.save()
 
-                messages.success(request, f'Your drink has been created')
+                messages.success(
+                    request, f'Your drink { drink.name } has been created!')
                 return redirect('profile_public', username=request.user.username)
     else:
         create_user_drink_form = CreateUserDrinkForm()
@@ -99,7 +120,7 @@ def profile_create_drink(request):
         instruction_formset = InstructionFormset(prefix='instruction')
 
     context = {
-        'profile': profile,
+        'profile': request.user,
         'create_user_drink_form': create_user_drink_form,
         'ingredient_formset': ingredient_formset,
         'instruction_formset': instruction_formset
@@ -110,7 +131,10 @@ def profile_create_drink(request):
 
 @login_required
 def profile_edit(request):
-    username = User.objects.get(username=request.user)
+    """
+    Profile Edit View
+    """
+
     if request.method == 'POST':
         user_update_form = UserUpdateForm(request.POST, instance=request.user)
         profile_update_form = ProfileUpdateForm(
@@ -126,7 +150,7 @@ def profile_edit(request):
         profile_update_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
-        'profile': username,
+        'profile': request.user,
         'user_update_form': user_update_form,
         'profile_update_form': profile_update_form
     }
@@ -135,6 +159,10 @@ def profile_edit(request):
 
 
 def profile_public(request, username):
+    """
+    Profile View
+    """
+
     username = User.objects.get(username=username)
     drinks = UserDrink.objects.filter(user=username).order_by('-timestamp')
     requests = (FriendRequest.objects.filter(requestee=request.user.profile) | FriendRequest.objects.filter(requestor=request.user.profile)) & (
@@ -169,6 +197,10 @@ def profile_public(request, username):
 
 @login_required
 def liked_drinks(request):
+    """
+    Liked Drinks View
+    """
+
     if request.user.is_authenticated:
         user = request.user
         profile = Profile.objects.get(user=user)
@@ -197,10 +229,18 @@ def liked_drinks(request):
 
 
 def about(request):
+    """
+    About View
+    """
+
     return render(request, 'gnt/about.html')
 
 
 def disliked_drinks(request):
+    """
+    Disliked Drinks View
+    """
+
     if request.user.is_authenticated:
         user = request.user
         profile = Profile.objects.get(user=user)
@@ -222,30 +262,47 @@ def disliked_drinks(request):
             context = {
                 'profile': request.user
             }
+
             return render(request, 'gnt/disliked_drinks.html', context)
     else:
         return HttpResponseRedirect('/home/')
 
 
 def timeline(request):
+    """
+    Timeline View
+    """
+
     drinks = UserDrink.objects.all().order_by('-timestamp')
+
     context = {
         'drinks': drinks
     }
+
     return render(request, 'gnt/timeline.html', context)
 
 
 def search(request):
+    """
+    Search View
+    """
+
     if request.method == 'POST':
         profiles = User.objects.filter(
             username__startswith=request.POST['search_input'])
+
         context = {
             'profiles': profiles
         }
+
         return render(request, 'gnt/search.html', context)
 
 
 def notifications(request, username):
+    """
+    Notifications View
+    """
+
     requests = FriendRequest.objects.filter(requestee=request.user.profile)
 
     if 'add-friend' in request.POST:
@@ -274,28 +331,17 @@ def notifications(request, username):
         'requests': requests
     }
 
-    # return HttpResponse('<h1>hello</h1>')
     return render(request, 'gnt/notifications.html', context)
 
 
 @login_required
 def friends(request, username):
+    """
+    Friends View
+    """
+
     if request.method == 'POST':
-
-        if 'add-friend' in request.POST:
-            requestor = User.objects.get(username=request.POST['requestor'])
-            friend_request = FriendRequest.objects.get(
-                requestor=requestor.profile, requestee=request.user.profile)
-            friend_request.delete()
-            friends = Friend()
-            friends.friend1 = request.user.profile
-            friends.friend2 = requestor.profile
-            friends.save()
-
-            messages.success(
-                request, f'you have successfully added friend { requestor.profile.user }')
-
-        elif 'remove-friend' in request.POST:
+        if 'remove-friend' in request.POST:
             requestor = User.objects.get(username=request.POST['requestor'])
             friends = Friend.objects.get(
                 friend1=requestor.profile, friend2=request.user.profile)
