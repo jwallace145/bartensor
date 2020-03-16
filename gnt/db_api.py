22dfd5366c878a8cca17a85df4dfc216aecf26f7
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-03-16 01:17:57 -0400

    fixed some old references to older db migrations

--- gnt/db_api.py
+++ gnt/db_api.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from .models import Profile, Drink, DrinkName, ProfileToLikedDrink, ProfileToDislikedDrink
+from gnt.models import Profile, Drink, DrinkName, ProfileToLikedDrink, ProfileToDislikedDrink
 
 
 def like_drink(request):
@@ -8,20 +8,20 @@ def like_drink(request):
         drink = Drink.objects.get(drink_hash=request.POST['drink_id'])
 
         profile = Profile.objects.get(id=request.user.profile.id)
+
         # Check to see if drink is disliked then remove
         disliked_drink = ProfileToDislikedDrink.objects.filter(
-            profile_FK=profile, drink_FK=drink)
+            profile=profile, drink=drink)
         if disliked_drink:
             disliked_drink.delete()
 
-        if ProfileToLikedDrink.objects.filter(profile_FK=profile, drink_FK=drink):
+        if ProfileToLikedDrink.objects.filter(profile=profile, drink=drink):
             response = {
                 'message': "Drink " + str(request.POST['drink_id']) + " has already been liked by " + str(username) + ". No changes to db",
                 'status': 422
             }
         else:
-            new_like = ProfileToLikedDrink(
-                profile_FK=profile, drink_FK=drink)
+            new_like = ProfileToLikedDrink(profile=profile, drink=drink)
             new_like.save()
             msg = "Drink " + \
                 str(request.POST['drink_id']) + "added to " + \
@@ -45,21 +45,22 @@ def dislike_drink(request):
         username = request.POST['user']
         drink = Drink.objects.get(drink_hash=request.POST['drink_id'])
         profile = Profile.objects.get(id=request.user.profile.id)
+
         # If user has liked drink, remove it from liked table
         liked_drink = ProfileToLikedDrink.objects.filter(
-            profile_FK=profile, drink_FK=drink)
+            profile=profile, drink=drink)
+
         if liked_drink:
             # Remove liked drink
             liked_drink.delete()
 
-        if ProfileToDislikedDrink.objects.filter(profile_FK=profile, drink_FK=drink):
+        if ProfileToDislikedDrink.objects.filter(profile=profile, drink=drink):
             response = {
                 'message': "Drink " + str(request.POST['drink_id']) + " has already been disliked by " + str(username) + ". No changes to db",
                 'status': 422
             }
         else:
-            new_dislike = ProfileToDislikedDrink(
-                profile_FK=profile, drink_FK=drink)
+            new_dislike = ProfileToDislikedDrink(profile=profile, drink=drink)
             new_dislike.save()
             msg = "Drink " + \
                 str(request.POST['drink_id']) + "added to " + \
@@ -84,7 +85,7 @@ def remove_liked_drink(request):
         drink = Drink.objects.get(drink_hash=request.POST['drink_id'])
         profile = Profile.objects.get(id=request.user.profile.id)
         liked_drink = ProfileToLikedDrink.objects.filter(
-            profile_FK=profile, drink_FK=drink)
+            profile=profile, drink=drink)
         if liked_drink:
             liked_drink.delete()
             response = {
@@ -112,7 +113,8 @@ def remove_disliked_drink(request):
         drink = Drink.objects.get(drink_hash=request.POST['drink_id'])
         profile = Profile.objects.get(id=request.user.profile.id)
         disliked_drink = ProfileToDislikedDrink.objects.filter(
-            profile_FK=profile, drink_FK=drink)
+            profile=profile, drink=drink)
+
         if disliked_drink:
             disliked_drink.delete()
             response = {
@@ -142,19 +144,19 @@ def get_liked_disliked_drinks(request):
         profile = Profile.objects.get(user=user)
         # get liked drinks
         profile_to_liked_drink = ProfileToLikedDrink.objects.filter(
-            profile_FK=profile.id)
+            profile=profile)
         if profile_to_liked_drink:
             response = [0 for i in range(len(profile_to_liked_drink))]
             for i, ptd in enumerate(profile_to_liked_drink):
-                response[i] = ptd.drink_FK.drink_hash
+                response[i] = ptd.drink.drink_hash
             liked_drinks = response
         # get disliked drinks
         profile_to_disliked_drink = ProfileToDislikedDrink.objects.filter(
-            profile_FK=profile.id)
+            profile=profile)
         if profile_to_disliked_drink:
             response = [0 for i in range(len(profile_to_disliked_drink))]
             for i, ptd in enumerate(profile_to_disliked_drink):
-                response[i] = ptd.drink_FK.drink_hash
+                response[i] = ptd.drink.drink_hash
             disliked_drinks = response
         # return response
         resp = {
--- gnt/management/commands/db-populate.py
+++ gnt/management/commands/db-populate.py
@@ -1,12 +1,11 @@
-from django.core.management.base import BaseCommand, CommandError
-from gnt.models import Drinks, Drink_names, User_drink
+from django.core.management.base import BaseCommand
+from gnt.models import Drink, DrinkName, UserDrink
 from ibm_watson import DiscoveryV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from django.conf import settings
-from django.contrib.auth.models import User, UserManager
+from django.contrib.auth.models import User
 import os
 from django.core.files import File
-
 
 
 class Command(BaseCommand):
@@ -14,44 +13,57 @@ class Command(BaseCommand):
     def _create_tags(self):
         environment_id = 'b7d1486c-2fdc-40c5-a2ce-2d78ec48fa76'
         collection_id = '7c11f329-5f31-4e59-aa63-fde1e91ff681'
-        authenticator = IAMAuthenticator(getattr(settings, 'WATSON_DISCOVERY_API_KEY', None))
-        discovery = DiscoveryV1(version='2019-04-30',authenticator=authenticator)
-        discovery.set_service_url('https://api.us-south.discovery.watson.cloud.ibm.com/')
-        response = discovery.query(environment_id, collection_id, _return=['id','names'], count='1000').result['results']
+        authenticator = IAMAuthenticator(
+            getattr(settings, 'WATSON_DISCOVERY_API_KEY', None))
+        discovery = DiscoveryV1(version='2019-04-30',
+                                authenticator=authenticator)
+        discovery.set_service_url(
+            'https://api.us-south.discovery.watson.cloud.ibm.com/')
+        response = discovery.query(environment_id, collection_id, _return=[
+                                   'id', 'names'], count='1000').result['results']
         for i in response:
             id_var = i['id']
             print(f'INSERTING {id_var}', file=self.stdout)
-            d = Drinks(drink_hash=id_var)
+            d = Drink(drink_hash=id_var)
             d.save()
             names_arr = i['names']
-            d = Drinks.objects.get(drink_hash=id_var)
+            d = Drink.objects.get(drink_hash=id_var)
             for name in names_arr:
                 print(f'INSERTING {str(name)}', file=self.stdout)
-                d_name = Drink_names(drink_FK = d, drink_name=name)
+                d_name = DrinkName(drink=d, drink_name=name)
                 d_name.save()
 
     def _create_fake_users(self):
-        User.objects.create_superuser(username="Caleb", email = "caleb@gmail.com", password='password')
+        User.objects.create_superuser(
+            username="Caleb", email="caleb@gmail.com", password='password')
         print(f'CREATED ADMIN ACCOUNT USERNAME: Caleb, PASSWORD: password')
-        User.objects.create_superuser(username="Jimmy", email = "jimmy@gmail.com", password='password')
+        User.objects.create_superuser(
+            username="Jimmy", email="jimmy@gmail.com", password='password')
         print(f'CREATED ADMIN ACCOUNT USERNAME: Jimmy, PASSWORD: password')
-        User.objects.create_superuser(username="Eric", email = "Eric@gmail.com", password='password')
+        User.objects.create_superuser(
+            username="Eric", email="Eric@gmail.com", password='password')
         print(f'CREATED ADMIN ACCOUNT USERNAME: Eric, PASSWORD: password')
-        User.objects.create_superuser(username="Michael", email = "Michael@gmail.com", password='password')
+        User.objects.create_superuser(
+            username="Michael", email="Michael@gmail.com", password='password')
         print(f'CREATED ADMIN ACCOUNT USERNAME: Michael, PASSWORD: password')
-        User.objects.create_superuser(username="Jack", email = "Jack@gmail.com", password='password')
+        User.objects.create_superuser(
+            username="Jack", email="Jack@gmail.com", password='password')
         print(f'CREATED ADMIN ACCOUNT USERNAME: Jack, PASSWORD: password')
-        drink_images_folder = [x[0] + '/' for x in os.walk('gnt/static/data/drink_images/')]
+        drink_images_folder = [
+            x[0] + '/' for x in os.walk('gnt/static/data/drink_images/')]
         drink_images_folder = drink_images_folder[2:]
         for i in range(100):
-            User.objects.create_user(username=f'User{i}', email=f'user{i}@gmail.com', password='password')
+            User.objects.create_user(
+                username=f'User{i}', email=f'user{i}@gmail.com', password='password')
             u = User.objects.get(username=f'User{i}')
-            print(f'CREATED ACCOUNT USERNAME: User{i}, PASSWORD: password', end='\t')
-            for img in os.listdir(drink_images_folder[i]): 
+            print(
+                f'CREATED ACCOUNT USERNAME: User{i}, PASSWORD: password', end='\t')
+            for img in os.listdir(drink_images_folder[i]):
                 path = drink_images_folder[i] + img
             im = open(path, 'rb')
             django_file = File(im)
-            user_d = User_drink(user=u, name=f'drink{i}', description=f'description{i}', image=django_file)
+            user_d = UserDrink(
+                user=u, name=f'drink{i}', description=f'description{i}', image=django_file)
             user_d.save()
             print(f'CREATED USER DRINK: drink{i}')
 
@@ -59,4 +71,3 @@ class Command(BaseCommand):
         self._create_fake_users()
         self._create_tags()
         print('TRANSACTION COMPLETE', file=self.stdout)
-        
