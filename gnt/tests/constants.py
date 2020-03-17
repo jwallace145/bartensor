1a40491908b90a1c3f25cf0c05c50392c2525193
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-03-17 01:32:36 -0400

    updated form tests

---
+++ gnt/tests/constants.py
@@ -0,0 +1,26 @@
+"""
+Test Constants Module
+"""
+
+# user model form constants
+TEST_USERNAME = 'testuser'
+TEST_EXISTING_USERNAME = 'existinguser'
+TEST_EMAIL = 'testuser@email.com'
+TEST_PASSWORD = 'TestPassword123'
+TEST_INCORRECT_PASSWORD = 'TestPassword321'
+
+# profile model form constants
+TEST_BIO = 'test bio'
+TEST_IMAGE = 'default.jpg'
+
+# user drink model form constants
+TEST_DRINK_NAME = 'test drink name'
+TEST_DRINK_DESCRIPTION = 'test drink description'
+TEST_DRINK_IMAGE = 'default.jpg'
+
+# user drink ingredient model form constants
+TEST_USER_DRINK_INGREDIENT_NAME = 'test ingredient name'
+TEST_USER_DRINK_INGREDIENT_QUANTITY = 'test ingredient quantity'
+
+# user drink instruction model form constants
+TEST_USER_DRINK_INSTRUCTION = 'test instruction'
--- gnt/tests/test_forms.py
+++ gnt/tests/test_forms.py
@@ -5,116 +5,17 @@ Forms Test Module
 # import necessary modules
 from django.contrib.auth.models import User
 from django.test import TestCase
-from gnt.forms import CreateUserDrinkForm, CreateUserDrinkIngredientForm, CreateUserDrinkInstructionForm, ProfileUpdateForm, UserRegisterForm, UserUpdateForm
-
-
-class UserRegisterFormTest(TestCase):
-    """
-    User Register Form Test Class
-    """
-
-    # constants
-    TEST_USERNAME = 'testuser'
-    TEST_EXISTING_USERNAME = 'existinguser'
-    TEST_EMAIL = 'testuser@email.com'
-    TEST_PASSWORD = 'TestPassword123'
-    TEST_INCORRECT_PASSWORD = 'TestPassword321'
-
-    def setUp(self):
-        """
-        Test Set Up
-        """
-
-        # populate test database with an existing user
-        User.objects.create(username=self.TEST_EXISTING_USERNAME,
-                            email=self.TEST_EMAIL, password=self.TEST_PASSWORD)
-
-    def test_user_register_form_success(self):
-        """
-        User Register Form Test Success
-        """
-
-        form = UserRegisterForm(data={
-            'username': self.TEST_USERNAME,
-            'email': self.TEST_EMAIL,
-            'password1': self.TEST_PASSWORD,
-            'password2': self.TEST_PASSWORD
-        })
-
-        self.assertTrue(form.is_valid())
-
-    def test_user_register_form_different_passwords(self):
-        """
-        User Register Form Test Failure Different Passwords
-        """
-
-        form = UserRegisterForm(data={
-            'username': self.TEST_USERNAME,
-            'email': self.TEST_EMAIL,
-            'password1': self.TEST_PASSWORD,
-            'password2': self.TEST_INCORRECT_PASSWORD
-        })
-
-        self.assertFalse(form.is_valid())
-
-    def test_duplicate_user_register_form(self):
-        """
-        User Register Form Test Failure Duplicate Users
-        """
-
-        form = UserRegisterForm(data={
-            'username': self.TEST_EXISTING_USERNAME,
-            'email': self.TEST_EMAIL,
-            'password1': self.TEST_PASSWORD,
-            'password2': self.TEST_INCORRECT_PASSWORD
-        })
-
-        self.assertFalse(form.is_valid())
-
-
-class UserUpdateFormTest(TestCase):
-    """
-    User Update Form Test Class
-    """
-
-    # constants
-    TEST_USERNAME = 'testuser'
-    TEST_EXISTING_USERNAME = 'existinguser'
-    TEST_EMAIL = 'testuser@email.com'
-
-    def test_user_update_form_success(self):
-        """
-        User Update Form Test Success
-        """
-
-        form = UserUpdateForm(data={
-            'username': self.TEST_USERNAME,
-            'email': self.TEST_EMAIL
-        })
-
-        self.assertTrue(form.is_valid())
-
-
-class ProfileUpdateFormTest(TestCase):
-    """
-    Profile Update Form Test Class
-    """
-
-    # constants
-    TEST_BIO = 'test bio'
-    TEST_IMAGE = 'default.jpg'
-
-    def test_profile_update_form_success(self):
-        """
-        Profile Update Form Test Success
-        """
-
-        form = ProfileUpdateForm(data={
-            'image': self.TEST_IMAGE,
-            'bio': self.TEST_BIO
-        })
-
-        self.assertTrue(form.is_valid())
+from gnt.forms import CreateUserDrinkForm
+from gnt.forms import CreateUserDrinkIngredientForm
+from gnt.forms import CreateUserDrinkInstructionForm
+from gnt.forms import ProfileUpdateForm
+from gnt.forms import UserRegisterForm
+from gnt.forms import UserUpdateForm
+from gnt.models import Ingredient
+from gnt.models import Instruction
+from gnt.models import Profile
+from gnt.models import UserDrink
+from gnt.tests import constants
 
 
 class CreateUserDrinkFormTest(TestCase):
@@ -122,23 +23,45 @@ class CreateUserDrinkFormTest(TestCase):
     Create User Drink Form Test Class
     """
 
-    # constants
-    TEST_DRINK_NAME = 'test drink name'
-    TEST_DRINK_DESCRIPTION = 'test drink description'
-    TEST_DRINK_IMAGE = 'default.jpg'
+    def setUp(self):
+        """
+        Test Set Up
+        """
+
+        # populate test database with an existing user
+        User.objects.create(
+            username=constants.TEST_EXISTING_USERNAME,
+            email=constants.TEST_EMAIL,
+            password=constants.TEST_PASSWORD
+        )
 
     def test_create_user_drink_form_success(self):
         """
-        Create User Drink Form Test Success
+        Successful Create User Drink Form Test
         """
 
+        # create user drink form
         form = CreateUserDrinkForm(data={
-            'name': self.TEST_DRINK_NAME,
-            'description': self.TEST_DRINK_DESCRIPTION,
-            'image': self.TEST_DRINK_IMAGE
+            'name': constants.TEST_DRINK_NAME,
+            'description': constants.TEST_DRINK_DESCRIPTION,
+            'image': constants.TEST_DRINK_IMAGE
         })
 
+        # assert the form is valid
         self.assertTrue(form.is_valid())
+
+        # save the user drink with the form data and referenced user
+        drink = form.save(commit=False)
+        user = User.objects.get(username=constants.TEST_EXISTING_USERNAME)
+        drink.user = user
+        drink.save()
+
+        # assert the drink fields are equal to the test constants
+        self.assertTrue(isinstance(drink, UserDrink))
+        self.assertEquals(user, drink.user)
+        self.assertEquals(constants.TEST_DRINK_NAME, drink.name)
+        self.assertEquals(constants.TEST_DRINK_DESCRIPTION, drink.description)
+        self.assertEquals(constants.TEST_DRINK_IMAGE, drink.image)
 
 
 class CreateUserDrinkIngredientFormTest(TestCase):
@@ -146,20 +69,30 @@ class CreateUserDrinkIngredientFormTest(TestCase):
     Create User Drink Ingredient Form Test Class
     """
 
-    # constants
-    TEST_USER_DRINK_INGREDIENT_NAME = 'test ingredient name'
-    TEST_USER_DRINK_INGREDIENT_QUANTITY = 'test ingredient quantity'
+    def setUp(self):
+        """
+        Test Set Up
+        """
+
+        # populate test database with an existing user
+        User.objects.create(
+            username=constants.TEST_EXISTING_USERNAME,
+            email=constants.TEST_EMAIL,
+            password=constants.TEST_PASSWORD
+        )
 
     def test_create_user_drink_ingredient_form_success(self):
         """
-        Create User Drink Ingredient Form Test Success
+        Successful Create User Drink Ingredient Form Test
         """
 
+        # create user drink ingredient form
         form = CreateUserDrinkIngredientForm(data={
-            'name': self.TEST_USER_DRINK_INGREDIENT_NAME,
-            'quantity': self.TEST_USER_DRINK_INGREDIENT_QUANTITY
+            'name': constants.TEST_USER_DRINK_INGREDIENT_NAME,
+            'quantity': constants.TEST_USER_DRINK_INGREDIENT_QUANTITY
         })
 
+        # assert the form is valid
         self.assertTrue(form.is_valid())
 
 
@@ -168,16 +101,182 @@ class CreateUserDrinkInstructionFormTest(TestCase):
     Create User Drink Instruction Form Test Class
     """
 
-    # constants
-    TEST_USER_DRINK_INSTRUCTION = 'test instruction'
+    def setUp(self):
+        """
+        Test Set Up
+        """
+
+        # populate test database with an existing user
+        User.objects.create(
+            username=constants.TEST_EXISTING_USERNAME,
+            email=constants.TEST_EMAIL,
+            password=constants.TEST_PASSWORD
+        )
 
     def test_create_user_drink_instruction_form_success(self):
         """
-        Create User Drink Instruction Form Test Success
+        Successful Create User Drink Instruction Form Test
         """
 
+        # create user drink instruction form
         form = CreateUserDrinkInstructionForm(data={
-            'instruction': self.TEST_USER_DRINK_INSTRUCTION
+            'instruction': constants.TEST_USER_DRINK_INSTRUCTION
         })
 
+        # assert the form is valid
         self.assertTrue(form.is_valid())
+
+
+class ProfileUpdateFormTest(TestCase):
+    """
+    Profile Update Form Test Class
+    """
+
+    def setUp(self):
+        """
+        Test Set Up
+        """
+
+        # populate test database with an existing user
+        User.objects.create(
+            username=constants.TEST_EXISTING_USERNAME,
+            email=constants.TEST_EMAIL,
+            password=constants.TEST_PASSWORD
+        )
+
+    def test_profile_update_form_success(self):
+        """
+        Profile Update Form Test Success
+        """
+
+        # create profile update form
+        form = ProfileUpdateForm(data={
+            'image': constants.TEST_IMAGE,
+            'bio': constants.TEST_BIO
+        })
+
+        # assert the form is valid
+        self.assertTrue(form.is_valid())
+
+
+class UserRegisterFormTest(TestCase):
+    """
+    User Register Form Test Class
+    """
+
+    def setUp(self):
+        """
+        Test Set Up
+        """
+
+        # populate test database with an existing user
+        User.objects.create(
+            username=constants.TEST_EXISTING_USERNAME,
+            email=constants.TEST_EMAIL,
+            password=constants.TEST_PASSWORD
+        )
+
+    def test_user_register_form_success(self):
+        """
+        Successful User Register Form Test
+        """
+
+        # create user register form
+        form = UserRegisterForm(data={
+            'username': constants.TEST_USERNAME,
+            'email': constants.TEST_EMAIL,
+            'password1': constants.TEST_PASSWORD,
+            'password2': constants.TEST_PASSWORD
+        })
+
+        # assert the form is valid
+        self.assertTrue(form.is_valid())
+
+    def test_user_register_form_different_passwords(self):
+        """
+        User Register Form Different Passwords Test
+        """
+
+        # create user register form
+        form = UserRegisterForm(data={
+            'username': constants.TEST_USERNAME,
+            'email': constants.TEST_EMAIL,
+            'password1': constants.TEST_PASSWORD,
+            'password2': constants.TEST_INCORRECT_PASSWORD
+        })
+
+        # assert the form is not valid
+        self.assertFalse(form.is_valid())
+
+    def test_duplicate_user_register_form(self):
+        """
+        User Register Form Existing User Test
+        """
+
+        # create user register form
+        form = UserRegisterForm(data={
+            'username': constants.TEST_EXISTING_USERNAME,
+            'email': constants.TEST_EMAIL,
+            'password1': constants.TEST_PASSWORD,
+            'password2': constants.TEST_PASSWORD
+        })
+
+        # assert the form is not valid
+        self.assertFalse(form.is_valid())
+
+
+class UserUpdateFormTest(TestCase):
+    """
+    User Update Form Test Class
+    """
+
+    def setUp(self):
+        """
+        Test Set Up
+        """
+
+        # populate test database with an existing user
+        User.objects.create(
+            username=constants.TEST_EXISTING_USERNAME,
+            email=constants.TEST_EMAIL,
+            password=constants.TEST_PASSWORD
+        )
+
+    def test_user_update_form_success(self):
+        """
+        Successful User Update Form Test
+        """
+
+        # create user update form
+        form = UserUpdateForm(data={
+            'username': constants.TEST_USERNAME,
+            'email': constants.TEST_EMAIL
+        })
+
+        # assert the form is valid
+        self.assertTrue(form.is_valid())
+
+        # save the form
+        form.save()
+
+        # retrieve updated user
+        user = User.objects.get(username=constants.TEST_USERNAME)
+
+        # assert the updated user fields are equal to the test constants
+        self.assertTrue(isinstance(user, User))
+        self.assertEquals(constants.TEST_USERNAME, user.username)
+        self.assertEquals(constants.TEST_EMAIL, user.email)
+
+    def test_user_update_form_existing_user(self):
+        """
+        User Update Form Existing User Test
+        """
+
+        # create user update form
+        form = UserUpdateForm(data={
+            'username': constants.TEST_EXISTING_USERNAME,
+            'email': constants.TEST_EMAIL
+        })
+
+        # assert the form is not valid
+        self.assertFalse(form.is_valid())
