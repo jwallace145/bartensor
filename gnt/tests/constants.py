a87fa82c1300c2a0578508bdba67fda0b210bb48
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-03-17 05:42:07 -0400

    added another selenium test for profile update

--- gnt/tests/constants.py
+++ gnt/tests/constants.py
@@ -6,11 +6,13 @@ Test Constants Module
 
 # profile model constants
 TEST_BIO = 'test bio'
+TEST_EXISTING_BIO = 'existing bio'
 TEST_IMAGE = 'default.jpg'
 
 # user model constants
 TEST_USERNAME = 'testuser'
 TEST_EXISTING_USERNAME = 'existinguser'
+TEST_EXISTING_EMAIL = 'existinguser@gmail.com'
 TEST_EMAIL = 'testuser@email.com'
 TEST_PASSWORD = 'TestPassword123'
 TEST_INCORRECT_PASSWORD = 'TestPassword321'
@@ -28,3 +30,13 @@ TEST_USER_DRINK_INGREDIENT_QUANTITY = 'test ingredient quantity'
 
 # user drink instruction model constants
 TEST_USER_DRINK_INSTRUCTION = 'test instruction'
+
+# selenium test constants
+ID_USERNAME = 'id_username'
+ID_EMAIL = 'id_email'
+ID_PASSWORD1 = 'id_password1'
+ID_PASSWORD2 = 'id_password2'
+ID_IMAGE = 'id_image'
+ID_BIO = 'id_bio'
+UPDATE_BUTTON = 'update'
+REGISTER_BUTTON = 'register'
