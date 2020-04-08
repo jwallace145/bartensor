b10fd50db64f48f2eff74769bca805749c115523
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-04-08 02:26:49 -0400

    changed database - not that tight

--- website/settings.py
+++ website/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bartensor',
+        'USER': 'bartensoradmin',
+        'PASSWORD': 'BartensorOSU!',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
