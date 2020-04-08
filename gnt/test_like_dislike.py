4ac58fee629f15d76b188572f2d6e82c15bd849c
Eric Thomas <bluedevils027@gmail.com>
2020-04-08 19:40:49 -0400

    fixed remove disliked drink path

---
+++ gnt/test_like_dislike.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from gnt import models
+
+
+class TestLikes(TestCase):
+"""
+Tests like drink functionality with DB
+"""
+    def create_test_Profile(self):
+        self.test_user = User.objects.create_user('Tester')
+        self.test_profile = models.Profile.objects.create(user=self.test_user, bio='test')
+
+    def new_user_1_like(self):
+        drink = Drinks.objects.get(drink_hash='1b588790-9610-478c-8899-e700d33f6eb5')
+        profile = Profile.objects.get(user=self.test_user)
