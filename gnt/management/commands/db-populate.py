92527152674b84036d3f3f8d68663f2cb397044b
Caleb Woy <woy.3@osu.edu>
2020-04-09 11:03:33 -0400

    db-populate

--- gnt/management/commands/db-populate.py
+++ gnt/management/commands/db-populate.py
@@ -93,64 +93,64 @@ class Command(BaseCommand):
         profile.save()
         print(f'CREATED ADMIN ACCOUNT USERNAME: Jack, PASSWORD: password')
 
-        drink_images_folder = [
-            x[0] + '/' for x in os.walk('gnt/static/data/drink_images/')]
-        drink_images_folder = drink_images_folder[2:]
-        blns_reader = open('gnt/static/data/blns.json', encoding='utf-8')
-        blns = json.load(blns_reader)
-        for i in range(0, 11):
-            User.objects.create_user(
-                username=f'User{i}', email=f'user{i}@gmail.com', password='password')
-            u = User.objects.get(username=f'User{i}')
-            print(f'CREATED ACCOUNT USERNAME: User{i}, PASSWORD: password', end='\t')
-            for img in os.listdir(drink_images_folder[i]):
-                path = drink_images_folder[i] + img
-            im = open(path, 'rb')
-            django_file = File(im)
-            user_d = UserDrink(
-                user=u, name=f'TestDrink{i}', votes=52, description=f'Description{i}')
-            try:
-                user_d.full_clean()
-                user_d.image.save(os.path.basename(path), django_file)
-                user_d.save()
-                print(f'CREATED USER DRINK: TestDrink{i}', end='\t')
-                d = UserDrink.objects.get(name=f'TestDrink{i}')
-                ingr = Ingredient(drink=d, name=f'Ingredient{i}', quantity=f'{i}')
-                ingr.save()
-                print(f'CREATED: Ingredient{i}', end='\t')
-                inst = Instruction(drink=d, instruction=f'Instruction{i}')
-                inst.save()
-                print(f'CREATED: Instruction{i}')
-            except:
-                print('NOT A VALID DRINK NAME OR DESCRIPTION')
-                pass
-            im.close()
-        for i in range(11, len(blns)):
-            User.objects.create_user(
-                username=f'User{i}', email=f'user{i}@gmail.com', password='password')
-            u = User.objects.get(username=f'User{i}')
-            print(f'CREATED ACCOUNT USERNAME: User{i}, PASSWORD: password', end='\t')
-            for img in os.listdir(drink_images_folder[i]):
-                path = drink_images_folder[i] + img
-            im = open(path, 'rb')
-            django_file = File(im)
-            user_d = UserDrink(
-                user=u, name=f'{blns[i]}', description=f'{blns[i]}')
-            try:
-                user_d.full_clean()
-                user_d.image.save(os.path.basename(path), django_file)
-                user_d.save()
-                try:
-                    print(f'CREATED USER DRINK: {blns[i]}')
-                except:
-                    print()
-                    pass
-            except:
-                print('NOT A VALID DRINK NAME OR DESCRIPTION')
-                pass
+        # drink_images_folder = [
+        #     x[0] + '/' for x in os.walk('gnt/static/data/drink_images/')]
+        # drink_images_folder = drink_images_folder[2:]
+        # blns_reader = open('gnt/static/data/blns.json', encoding='utf-8')
+        # blns = json.load(blns_reader)
+        # for i in range(0, 11):
+        #     User.objects.create_user(
+        #         username=f'User{i}', email=f'user{i}@gmail.com', password='password')
+        #     u = User.objects.get(username=f'User{i}')
+        #     print(f'CREATED ACCOUNT USERNAME: User{i}, PASSWORD: password', end='\t')
+        #     for img in os.listdir(drink_images_folder[i]):
+        #         path = drink_images_folder[i] + img
+        #     im = open(path, 'rb')
+        #     django_file = File(im)
+        #     user_d = UserDrink(
+        #         user=u, name=f'TestDrink{i}', votes=52, description=f'Description{i}')
+        #     try:
+        #         user_d.full_clean()
+        #         user_d.image.save(os.path.basename(path), django_file)
+        #         user_d.save()
+        #         print(f'CREATED USER DRINK: TestDrink{i}', end='\t')
+        #         d = UserDrink.objects.get(name=f'TestDrink{i}')
+        #         ingr = Ingredient(drink=d, name=f'Ingredient{i}', quantity=f'{i}')
+        #         ingr.save()
+        #         print(f'CREATED: Ingredient{i}', end='\t')
+        #         inst = Instruction(drink=d, instruction=f'Instruction{i}')
+        #         inst.save()
+        #         print(f'CREATED: Instruction{i}')
+        #     except:
+        #         print('NOT A VALID DRINK NAME OR DESCRIPTION')
+        #         pass
+        #     im.close()
+        # for i in range(11, len(blns)):
+        #     User.objects.create_user(
+        #         username=f'User{i}', email=f'user{i}@gmail.com', password='password')
+        #     u = User.objects.get(username=f'User{i}')
+        #     print(f'CREATED ACCOUNT USERNAME: User{i}, PASSWORD: password', end='\t')
+        #     for img in os.listdir(drink_images_folder[i]):
+        #         path = drink_images_folder[i] + img
+        #     im = open(path, 'rb')
+        #     django_file = File(im)
+        #     user_d = UserDrink(
+        #         user=u, name=f'{blns[i]}', description=f'{blns[i]}')
+        #     try:
+        #         user_d.full_clean()
+        #         user_d.image.save(os.path.basename(path), django_file)
+        #         user_d.save()
+        #         try:
+        #             print(f'CREATED USER DRINK: {blns[i]}')
+        #         except:
+        #             print()
+        #             pass
+        #     except:
+        #         print('NOT A VALID DRINK NAME OR DESCRIPTION')
+        #         pass
 
-            im.close()
-        blns_reader.close()
+        #     im.close()
+        # blns_reader.close()
 
     def handle(self, *args, **options):
         os.system('bash deldb.sh')
