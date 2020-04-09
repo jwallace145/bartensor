0f691320f87cd0f7ba1447a9113a7e1cd249a1c3
Eric-Thomas <bluedevils027@gmail.com>
2020-04-09 11:21:04 -0400

    fixed indent error

--- gnt/test_like_dislike.py
+++ gnt/test_like_dislike.py
@@ -4,13 +4,16 @@ from gnt import models
 
 
 class TestLikes(TestCase):
-"""
-Tests like drink functionality with DB
-"""
+    """
+    Tests like drink functionality with DB
+    """
+
     def create_test_Profile(self):
         self.test_user = User.objects.create_user('Tester')
-        self.test_profile = models.Profile.objects.create(user=self.test_user, bio='test')
+        self.test_profile = models.Profile.objects.create(
+            user=self.test_user, bio='test')
 
     def new_user_1_like(self):
-        drink = Drinks.objects.get(drink_hash='1b588790-9610-478c-8899-e700d33f6eb5')
+        drink = Drinks.objects.get(
+            drink_hash='1b588790-9610-478c-8899-e700d33f6eb5')
         profile = Profile.objects.get(user=self.test_user)
